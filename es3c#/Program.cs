e052aefdda11c867734c270da0929dfcb86de352
Luciano <lunick997@gmail.com>
2023-11-09 14:14:50 +0100

    commit

---
+++ es3c#/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("inserisci due numeri");
+string? num1 = Console.ReadLine();
+string? num2 = Console.ReadLine();
+int parsed1 = int.Parse(num1);
+int parsed2 = int.Parse(num2);
+int result = parsed1 + parsed2;
+Console.WriteLine($"il risultato è {result}");
